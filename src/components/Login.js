9e5e47b22ae713d4f8490b7845da26c2d18b7e01
Saket Karn <saketkarn38@gmail.com>
2024-12-06 23:05:01 +0530

    Till firebase hosting

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,31 @@
-import React, { useRef, useState } from 'react';
-import Header from './Header';
-import { checkValidData } from '../utils/validate';
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
-  const [isSignInForm, setIsSignInForm]=useState(true)
-  const [errorMessage, setErrorMessage]= useState(null)
-  const email = useRef(null)
-  const password = useRef(null)
-  const name=useRef(null)
-  const toggleSignInForm=()=>{
-    setIsSignInForm(!isSignInForm)
-  }
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
 
-  const handleButtonClick=()=>{
+  const handleButtonClick = () => {
     //First validate the form data
-    
+
     // console.log(email.current.value)
     // console.log(password.current.value)
 
-    const errMessage=checkValidData(email.current.value, password.current.value, name.current.value)
-    setErrorMessage(errMessage)
+    const errMessage = checkValidData(
+      email.current.value,
+      password.current.value,
+      name.current.value
+    );
+    setErrorMessage(errMessage);
     // console.log(errMessage)
-  }
+  };
   return (
     <div className="relative flex flex-col min-h-screen overflow-hidden">
       <Header />
@@ -29,18 +33,48 @@ const Login = () => {
         className="absolute inset-0 object-cover w-full h-full"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/655a9668-b002-4262-8afb-cf71e45d1956/5ff265b6-3037-44b2-b071-e81750b21783/IN-en-20240715-POP_SIGNUP_TWO_WEEKS-perspective_WEB_c6d6616f-4478-4ac2-bdac-f54b444771dd_large.jpg"
         alt="Login Background"
-        style={{ objectFit: 'cover' }}
+        style={{ objectFit: "cover" }}
       />
       <div className="flex-1 flex items-center justify-center">
-        <form onSubmit={(e)=> e.preventDefault()} className='w-3/12 absolute p-12 bg-black bg-opacity-70 rounded text-white'>
-          <h1 className='font-bold text-3xl py-4 px-2'>{isSignInForm?"Sign In":"Sign Up"}</h1>
-          {!isSignInForm && (<input ref={name} type="text" placeholder='Full Name' className='p-2 my-4 block w-full bg-black bg-opacity-40 border border-white-500'/>)}
-          <input ref={email} type="text" placeholder='Email' className='p-2 my-4 block w-full bg-black bg-opacity-40 border border-white-500'/>
-          <input ref={password} type="password" placeholder='Password' className='p-2 my-4 block w-full bg-black bg-opacity-40 border border-white-500' />
-          <p className='text-red-500 font-bold text-lg py-1'>{errorMessage}</p>
-          <button className='p-3 my-4 w-full bg-red-700 text-white rounded bg-black' onClick={handleButtonClick}>
-            {isSignInForm?"Sign In":"Sign Up"}</button>
-          <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm?"New to Netflix? Sign up now.":"Already Registered? Sign In Now."}</p>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="w-3/12 absolute p-12 bg-black bg-opacity-70 rounded text-white"
+        >
+          <h1 className="font-bold text-3xl py-4 px-2">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSignInForm && (
+            <input
+              ref={name}
+              type="text"
+              placeholder="Full Name"
+              className="p-2 my-4 block w-full bg-black bg-opacity-40 border border-white-500"
+            /> 
+          )}
+          <input
+            ref={email}
+            type="text"
+            placeholder="Email"
+            className="p-2 my-4 block w-full bg-black bg-opacity-40 border border-white-500"
+          />
+          <input
+            ref={password}
+            type="password"
+            placeholder="Password"
+            className="p-2 my-4 block w-full bg-black bg-opacity-40 border border-white-500"
+          />
+          <p className="text-red-500 font-bold text-lg py-1">{errorMessage}</p>
+          <button
+            className="p-3 my-4 w-full bg-red-700 text-white rounded bg-black"
+            onClick={handleButtonClick}
+          >
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+            {isSignInForm
+              ? "New to Netflix? Sign up now."
+              : "Already Registered? Sign In Now."}
+          </p>
         </form>
       </div>
     </div>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6kIZD6EqWk4D19JIHT99LspAgjmTH6_0",
+  authDomain: "netflixgpt-8bd6a.firebaseapp.com",
+  projectId: "netflixgpt-8bd6a",
+  storageBucket: "netflixgpt-8bd6a.firebasestorage.app",
+  messagingSenderId: "835221257907",
+  appId: "1:835221257907:web:8318308ff3a0c2bde8fac6",
+  measurementId: "G-VVK310M4Z3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
